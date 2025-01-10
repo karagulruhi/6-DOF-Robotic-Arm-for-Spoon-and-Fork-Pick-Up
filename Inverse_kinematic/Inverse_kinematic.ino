cb221efe6d4896168c30e6d331d19e34629d54cd
karagulruhi <karagulruhii@gmail.com>
2025-01-10 03:14:14 +0300

    Model optimized for Raspberry Pi deployment by converting to TFLite format.

---
+++ Inverse_kinematic/Inverse_kinematic.ino
@@ -0,0 +1,189 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#define DEBUG
+// PWM sürücüsünü oluştur
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // Minimum darbe genişliği (out of 4096)
+#define SERVOMAX  600 // Maksimum darbe genişliği (out of 4096)
+#define SERVO_FREQ 60  // Analog servolar ~50 Hz'de çalışır
+
+uint8_t servonum1 = 15;
+uint8_t servonum2 =14;
+uint8_t servonum3 = 13; 
+uint8_t servonum4 = 11; 
+uint8_t servonum5 = 10; 
+uint8_t servonum6 = 8; //gripper
+
+float Pi = 3.14;     // π取值
+float L0 = 60 + 30;  // 30为机械臂底部圆盘距离检测边缘距离，根据实际调整,60为圆盘底座固定值。
+float L1 = 72;       // 抓取物体表面到第二关节位置高度  unit:mm     72为默认高度
+float L2 = 105;      // 第2个关节到第3个关节长度
+float L3 = 128;      // 第3关节到第4关节的长度  145
+float L4 = 180;      // 第4个关节到手臂尖端的长度（夹具）,包含手爪旋转舵机180
+
+float X_EE, Y_EE, Z_EE;  // 手爪的x轴坐标-左右旋转,y轴坐标-前后伸展,z轴坐标-高度
+float Zoffset, D, d, R;
+float alpha1, alpha2, alpha3;
+float Theta_1, Theta_2, Theta_3, Theta_4;
+void Inverse_kinematics(double X_EE, double Y_EE, double Z_EE) {  // 机械臂逆运动学，给定X(左右),Y（前后）,Z（高度）坐标
+  D = sqrt(pow(X_EE, 2) + pow(Y_EE, 2));
+  if ((D > L4 + L0 || D < L4 + L0) && Z_EE > (L1 + L2 - L4)) {
+    if (X_EE > 0 && Z_EE >= L1) {
+      Theta_1 = (atan(Y_EE / X_EE)) * (180.00 / Pi);  //theta 1
+      d = D - L4;
+      Zoffset = Z_EE - L1;
+      R = sqrt(pow(d, 2) + pow(Zoffset, 2));
+      alpha1 = (acos(d / R)) * (180.00 / Pi);
+      alpha2 = (acos((pow(L2, 2) + pow(R, 2) - pow(L3, 2)) / (2 * L2 * R))) * (180.00 / Pi);
+      Theta_2 = (alpha1 + alpha2);                                                                //theta 2
+      Theta_3 = ((acos((pow(L2, 2) + pow(L3, 2) - pow(R, 2)) / (2 * L2 * L3))) * (180.00 / Pi));  //theta 3
+      alpha3 = 180.00 - ((180.00 - (alpha2 + Theta_3)) - alpha1);                                 //alpha 3
+      Theta_4 = 180 + 90 - alpha3;                                                                //theta 4
+    } else if (X_EE > 0 && Z_EE <= L1) {
+      Theta_1 = (atan(Y_EE / X_EE)) * (180.00 / Pi);  //theta 1
+      d = D - L4;
+      Zoffset = Z_EE - L1;
+      R = sqrt(pow(d, 2) + pow(Zoffset, 2));
+      alpha1 = (acos(d / R)) * (180.00 / Pi);
+      alpha2 = (acos((pow(L2, 2) + pow(R, 2) - pow(L3, 2)) / (2 * L2 * R))) * (180.00 / Pi);
+      Theta_2 = (alpha2 - alpha1);                                                                //theta 2
+      Theta_3 = ((acos((pow(L2, 2) + pow(L3, 2) - pow(R, 2)) / (2 * L2 * L3))) * (180.00 / Pi));  //theta 3
+      alpha3 = 180.00 - ((180.00 - (alpha2 + Theta_3)) + alpha1);                                 //alpha 3
+      Theta_4 = 180 + 90 - alpha3;                                                                //theta 4
+    } else if (X_EE == 0 && Z_EE >= L1) {
+      Theta_1 = 90.00;  //theta 1
+      d = D - L4;
+      Zoffset = Z_EE - L1;
+      R = sqrt(pow(d, 2) + pow(Zoffset, 2));
+      alpha1 = (acos(d / R)) * (180.00 / Pi);
+      alpha2 = (acos((pow(L2, 2) + pow(R, 2) - pow(L3, 2)) / (2 * L2 * R))) * (180.00 / Pi);
+      Theta_2 = alpha1 + alpha2;                                                                  //theta 2
+      Theta_3 = ((acos((pow(L2, 2) + pow(L3, 2) - pow(R, 2)) / (2 * L2 * L3))) * (180.00 / Pi));  //theta 3
+      alpha3 = 180.00 - ((180.00 - (alpha2 + Theta_3)) - alpha1);                                 //alpha 3
+      Theta_4 = 180 + 90 - alpha3;                                                                //theta 4
+    } else if (X_EE == 0 && Z_EE <= L1) {
+      Theta_1 = 90.00;  //theta 1
+      d = D - L4;
+      Zoffset = Z_EE - L1;
+      R = sqrt(pow(d, 2) + pow(Zoffset, 2));
+      alpha1 = (acos(d / R)) * (180.00 / Pi);
+      alpha2 = (acos((pow(L2, 2) + pow(R, 2) - pow(L3, 2)) / (2 * L2 * R))) * (180.00 / Pi);
+      Theta_2 = (alpha2 - alpha1);                                                                //theta 2
+      Theta_3 = ((acos((pow(L2, 2) + pow(L3, 2) - pow(R, 2)) / (2 * L2 * L3))) * (180.00 / Pi));  //theta 3
+      alpha3 = 180.00 - ((180.00 - (alpha2 + Theta_3)) + alpha1);                                 //alpha 3
+      Theta_4 = 180 + 90 - alpha3;                                                                //theta 4
+    } else if (X_EE < 0 && Z_EE >= L1) {
+      Theta_1 = 90.00 + (90.00 - abs((atan(Y_EE / X_EE)) * (180.00 / Pi)));  //theta 1
+      d = D - L4;
+      Zoffset = Z_EE - L1;
+      R = sqrt(pow(d, 2) + pow(Zoffset, 2));
+      alpha1 = (acos(d / R)) * (180.00 / Pi);
+      alpha2 = (acos((pow(L2, 2) + pow(R, 2) - pow(L3, 2)) / (2 * L2 * R))) * (180.00 / Pi);
+      Theta_2 = (alpha1 + alpha2);                                                                //theta 2
+      Theta_3 = ((acos((pow(L2, 2) + pow(L3, 2) - pow(R, 2)) / (2 * L2 * L3))) * (180.00 / Pi));  //theta 3
+      alpha3 = 180.00 - ((180.00 - (alpha2 + Theta_3)) - alpha1);                                 //alpha 3
+      Theta_4 = 180 + 90 - alpha3;                                                                //theta 4
+    } else if (X_EE < 0 && Z_EE <= L1) {
+      Theta_1 = 90.00 + (90.00 - abs((atan(Y_EE / X_EE)) * (180.00 / Pi)));  //theta 1
+      d = D - L4;
+      Zoffset = Z_EE - L1;
+      R = sqrt(pow(d, 2) + pow(Zoffset, 2));
+      alpha1 = (acos(d / R)) * (180.00 / Pi);
+      alpha2 = (acos((pow(L2, 2) + pow(R, 2) - pow(L3, 2)) / (2 * L2 * R))) * (180.00 / Pi);
+      Theta_2 = (alpha2 - alpha1);                                                                //theta 2
+      Theta_3 = ((acos((pow(L2, 2) + pow(L3, 2) - pow(R, 2)) / (2 * L2 * L3))) * (180.00 / Pi));  //theta 3
+      alpha3 = 180.00 - ((180.00 - alpha2 - Theta_3) + alpha1);                                   //alpha 3
+      Theta_4 = 180 + 90 - alpha3;                                                                //theta 4
+    }
+  }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Servo kontrol test!");
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ); // Analog servolar için frekansı ayarla
+
+  delay(10);
+
+
+}
+uint8_t servos[][3] = {
+  {0, 0, 180},        // Servo 1: 0° - 180°
+  {1, 0, 170},        // Servo 2: 0° - 170°
+  {2, 0, 174.4},      // Servo 3: 0° - 174.4°
+  {3, 19.44, 161.76}, // Servo 4: 19.44° - 161.76°
+  {4, 9, 180},
+  {6, 0, 180}          // Servo 5: 9° - 180°
+};
+
+void setServoAngle(uint8_t servo, uint8_t angle) {
+  // Servo'nun min ve max açılarını al
+
+  // Açıyı PWM'e dönüştür
+  uint16_t pulselen = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+
+  // PWM değerini servo motoruna gönder
+  pwm.setPWM(servo, 0, pulselen); // servos[servo][0] servo motorunun pin numarasını belirtir
+}
+
+
+int x= -130;
+int y= 350;
+
+int z= 150;
+int x_max=370;
+int x_min=250;
+int y_min=250;
+int y_max=370;
+int xy_max=20;
+void kinematic_verse(){
+  Theta_1 = constrain(Theta_1, 0, 180);
+  Theta_2 = constrain(Theta_2, 0, 172);
+  Theta_3 = constrain(Theta_3, 0, 178);
+  Theta_4 = constrain(Theta_4, 20, 170);
+  
+  
+ 
+
+  Inverse_kinematics(x,y, z);
+  setServoAngle(servonum1,Theta_1);
+  delay(10);
+  setServoAngle(servonum2,Theta_2);
+  delay(10);
+  setServoAngle(servonum3, 180-Theta_3);
+  delay(10);
+  setServoAngle(servonum4,0);
+  delay(10);
+
+  setServoAngle(servonum5, 90);
+ 
+  delay(10);
+
+}
+
+
+void loop() {
+  kinematic_verse();
+  
+
+
+
+
+
+
+  #ifdef DEBUG
+  Serial.print(" Servo: ");
+  Serial.print(Theta_1);
+  Serial.print("\t");
+  Serial.print(Theta_2);
+  Serial.print("\t");
+  Serial.print(Theta_3);
+  Serial.print("\t");
+  Serial.println(Theta_4);
+  #endif
+} 
